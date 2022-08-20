3e8ba4de51c9f0c781b2ec87ab6e3ff13e90dbd3
akm-ksk <akmmst0496@gmail.com>
2022-08-20 16:08:49 +0900

    レンダリングの最適化 コミット漏れ

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,62 @@
 import './App.css';
-import React, {useEffect, useState} from "react";
+import React, {memo, useCallback, useEffect, useState} from "react";
 import {ColoredMessage} from "./components/ColoredMessage";
 import {CssModule} from "./components/CssModule";
 import {StyledJSX} from "./components/Styled";
 import {StyledComponents} from "./components/StyledComponents";
 import {Emotion} from "./components/Emotion";
 import {TailwindCss} from "./components/TailwindCss";
+import {Child1} from "./components/Child1";
+import {Child4} from "./components/Child4";
 
 
-export const App = () => {
-    const [num, setNum] = useState(0);
+export const App = memo(
+    () => {
+        const [num, setNum] = useState(0);
+        const [childNum, setChildNum] = useState(0);
 
-    useEffect(() => {
-        console.log('num is change')
-    }, [num])
+        useEffect(() => {
+            console.log('num is change')
+        }, [num]);
 
-    const onClickButton = () => {
-        // setNum(num + 1);
-        setNum((prev) => prev + 1);
-    }
+        const onClickButton = () => {
+            // setNum(num + 1);
+            setNum((prev) => prev + 1);
+        };
 
-    return (
-        <>
-            <h1 style={{color: 'red'}}>Hello World</h1>
-            <ColoredMessage color="blue">React App</ColoredMessage>
-            <ColoredMessage color="pink">Java Script</ColoredMessage>
-            <button onClick={onClickButton}>ボタン</button>
-            <p>{num}</p>
+        const onClickChildButton = () => {
+            setChildNum((prev) => prev + 1);
+        };
 
-            <CssModule message="node sass"/>
-            <StyledJSX message="Styled Js"/>
-            <StyledComponents title="Styled Components"/>
-            <Emotion title="Emotion"/>
-            <TailwindCss title="Tailwind CSS"/>
-        </>
-    );
-}
+        const onClickRest = useCallback(
+            () => {
+                setChildNum(0)
+            }, []);
+
+        console.log("Appレンダリング");
+
+        return (
+            <>
+                <h1 style={{color: 'red'}}>Hello World</h1>
+                <ColoredMessage color="blue">React App</ColoredMessage>
+                <ColoredMessage color="pink">Java Script</ColoredMessage>
+                <button onClick={onClickButton}>ボタン</button>
+                <p>{num}</p>
+
+                <CssModule message="node sass"/>
+                <StyledJSX message="Styled Js"/>
+                <StyledComponents title="Styled Components"/>
+                <Emotion title="Emotion"/>
+                <TailwindCss title="Tailwind CSS"/>
+
+                <div>
+                    <button onClick={onClickChildButton}>ChildButton</button>
+                    <p>{childNum}</p>
+                    <Child1 onClickReset={onClickRest}/>
+                    <Child4/>
+                </div>
+            </>
+        )
+    });
 
 export default App;
